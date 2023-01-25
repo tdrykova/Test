42884a1d286d9206bc8729db80fb5d3980b1e018
Rykova Tatyana <tdrykova@edu.hse.ru>
2023-01-25 12:18:55 +0300

    sms

--- app/src/main/java/ru/tdrykova/myapplication/App.kt
+++ app/src/main/java/ru/tdrykova/myapplication/App.kt
@@ -3,4 +3,7 @@ package ru.tdrykova.myapplication
 import android.app.Application
 
 class App: Application() {
+    override fun getClassLoader(): ClassLoader {
+        return super.getClassLoader()
+    }
 }
